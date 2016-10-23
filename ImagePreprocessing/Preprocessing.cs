2d7359cd3a996c50b2f89f116b61f30debf7b615
st011594 <st011594@student.spbu.ru>
2016-10-23 15:32:01 +0300

    Added method countAreas in Preprocessing

--- ImagePreprocessing/Preprocessing.cs
+++ ImagePreprocessing/Preprocessing.cs
@@ -8,8 +8,8 @@ namespace ImagePreprocessing
 {
     public class Preprocessing
     {
-        const int IMAGE_WIDTH = 22;
-        const int IMAGE_HEIGHT = 22;
+        const int IMAGE_WIDTH = 32;
+        const int IMAGE_HEIGHT = 32;
 
         private int[,] image;
         private int[,] finalImage;
@@ -17,7 +17,19 @@ namespace ImagePreprocessing
         private int imageHeight;
         private int imageWidth;
 
-        private int numAreas;
+        public int numAreas;
+        private int segmentedPoints = 0;
+
+        private struct Point
+        {
+            public int X, Y;
+
+            public Point(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+        }
 
         public int[,] preprocessing(int[,] _image, int _imageWidth, int _imageHeight)
         {
@@ -28,7 +40,8 @@ namespace ImagePreprocessing
             return finalImage;
         }
 
-        public void resize()
+        // масштабирование
+        private void resize()
         {
             double stepX = (double)IMAGE_HEIGHT / imageHeight;
             double stepY = (double)IMAGE_WIDTH / imageWidth;
@@ -46,7 +59,7 @@ namespace ImagePreprocessing
         }
 
         //утолщение цифр 
-        public void thickening()
+        private void thickening()
         {
             bool isAlone;
 
@@ -81,9 +94,82 @@ namespace ImagePreprocessing
                 }
         }
 
+        //наращивает связную область из центральной точки
+        private void segmentImage(int[,] segmentedImage, int label)
+        {
+            int startX = 0;
+            int startY = 0;
+            bool isFind = false;
+
+
+            for (int i = 0; i < IMAGE_HEIGHT; i++)
+            {
+                for (int j = 0; j < IMAGE_WIDTH; j++)
+                    if (segmentedImage[i, j] < 2)
+                    {
+                        startX = i;
+                        startY = j;
+                        isFind = true;
+                        break;
+                    }
+                if (isFind) break;
+            }
+
+            if (isFind)
+            {
+
+                Point start = new Point(startX, startY);
+                int startValue = segmentedImage[start.X, start.Y];
+                Point current;
+
+                int[] neighbX = { -1, 1, 0, 0, -1, 1, -1, 1 };
+                int[] neighbY = { 0, 0, -1, 1, -1, -1, 1, 1 };
+
+                List<Point> points = new List<Point>();
+
+                points.Add(start);
+
+                while (points.Count() > 0)
+                {
+                    current = new Point(points.Last().X, points.Last().Y);
+
+                    points.Remove(points.Last());
+
+                    for (int i = 0; i < 8; i++)
+                    {
+
+                        Point p = new Point(current.X + neighbX[i], current.Y + neighbY[i]);
+
+                        if ((p.X >= 0) && (p.X < IMAGE_WIDTH) && (p.Y >= 0) && (p.Y < IMAGE_HEIGHT)
+                                       && (segmentedImage[p.X, p.Y] == startValue))
+                            points.Add(p);
+                    }
+
+                    segmentedImage[current.X, current.Y] = label;
+                }
+
+                for (int i = 0; i < IMAGE_HEIGHT; i++)
+                    for (int j = 0; j < IMAGE_WIDTH; j++)
+                        if (segmentedImage[i, j] == label)
+                            segmentedPoints++;
+            }
+
+        }
+
+        // считает связные области (хз зачем, но пусть будет, может пригодится)
         private int countAreas()
         {
-            return 0;
+            int[,] segmentedImage = (int[,])finalImage.Clone();
+
+            int label = 2;
+
+            while (segmentedPoints < IMAGE_HEIGHT * IMAGE_WIDTH)
+            {
+                label++;
+                segmentImage(segmentedImage, label);
+            }
+
+            return label - 2;
         }
     }
 }
